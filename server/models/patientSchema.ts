27b65beab7ed69079db1d55739dc6f4087b3f8c4
Domains18 <gibsonsgibson88@gmail.com>
2023-01-30 00:38:23 +0300

    rest api

--- server/models/patientSchema.ts
+++ server/models/patientSchema.ts
@@ -34,11 +34,19 @@ const patientSchema = new Schema({
         type: Boolean,
         required: true,
         default: false,
-        visited: {
-            type: Date,
-            Reason: {
+        visited: [{
+            date: {
+                type: Date,
+                required: true
+            },
+            doctor: {
                 type: String,
+                required: true
+            },
+            reason: {
+                type: String,
+                required: true
             }
-        }
+        }]
     }
 });
