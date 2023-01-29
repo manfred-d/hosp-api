36b7a4971c2b93ffd99b3740ec894bb51da078cb
Domains18 <gibsonsgibson88@gmail.com>
2023-01-29 21:10:27 +0300

    file structure

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,11 @@
 //making the hospital api
+const express = require('express');
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8000;
+const server = express();
+
+
+server.listen(`Listening on port ${PORT}`)
