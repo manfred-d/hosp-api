54d81d082aa96e177f05c34ffc4efcd7335f9fdf
Domains18 <gibsonsgibson88@gmail.com>
2023-01-29 22:15:22 +0300

    patient

--- server/models/patientSchema.ts
+++ server/models/patientSchema.ts
@@ -2,5 +2,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const patientSchema = new Schema({
-    
+    patientName: {
+        type: String,
+        required: true
+    },
+    NationalId: {
+        type: Number,
+        required: true
+    },
+    patientAge: {
+        type: Number,
+        required: true
+    },
+    patientGender: {
+        type: String,
+        required: true
+    },
+    patientAddress: {
+        type: String,
+        required: true
+    },
+    patientPhone: {
+        type: Number,
+        required: true
+    },
+    patientEmail: {
+        type: String,
+        required: false
+    },
+    visitedBefore: {
+        type: Boolean,
+        required: true
+    }
 });
