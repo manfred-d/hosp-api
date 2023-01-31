73adf0a41fe22df4caab9e209c8354eeb5366415
Domains18 <gibsonsgibson88@gmail.com>
2023-02-01 00:10:57 +0300

    patient information system

--- server/controllers/patientController.js
+++ server/controllers/patientController.js
@@ -46,4 +46,43 @@ const deletePatient = asyncHandler(async (req, res) => {
         res.json({ message: "failed to delete patient", error });
     }
 });
+//get all patients
+const getAllPatients = asyncHandler(async (req, res) => {
+    try {
+        const patients = await Patient.find({});
+        res.status(200).json({ patients });
+    } catch (error) {
+        return res.status(408);
+        res.json({ message: "failed to get patients", error });
+    }
+});
+//get single patient
+const getPatientById = asyncHandler(async (req, res) => {
+    const { nationalId } = req.body;
+    try {
+        const patients = Patient.findOne({ nationalId });
+        res.status(200).json({ patients });
+    } catch (error) {
+        return res.status(408);
+        res.json({ message: "failed to get patient", error });
+    }
+});
+//update patient
+const updatePatient = asyncHandler(async (req, res) => {
+    const { patientName, nationalId, patientAge, patientGender, patientPhone, patientEmail, visitedBefore } = req.body;
+    try {
+        const patientToUpdate = await Patient.findOneAndUpdate(
+            { nationalId },
+            {
+                $set: {
+                    patientName, nationalId, patientAge, patientGender, patientPhone, patientEmail, visitedBefore
+                }
+            });
+        res.status(200).json({ message: 'Patient updated successfully' });
+    } catch (error) {
+        return res.status(408);
+        res.json({ message: "failed to update patient", error });
+    }
+});
+
 module.exports = { newPatient };
