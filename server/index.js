0033c9bb6ca95b0972c063955f8219a5859bcc2b
Domains18 <gibsonsgibson88@gmail.com>
2023-02-04 17:18:24 +0300

    cleaning up

--- server/index.js
+++ server/index.js
@@ -7,13 +7,12 @@ const colors = require("colors");
 const mongoose = require("mongoose");
 const {errorHandler} = require("./handlers/errorHandler");
 const PORT = process.env.PORT;
-// console.log(process.env.PORT);
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 server.use(errorHandler);
-// server.use(express.static(path.join(__dirname, "client/build")));
+
 
 
 server.listen(PORT, ()=>{ console.log(`Listening on port ${PORT}`.yellow.underline.underline.bold)});
--- server/routes/patientRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const bodyParser = require('body-parser');
-
-module.exports = function (app) {
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true}))   
-}
