576eac97908a23e463eb05add5c51a7716e42abb
Domains18 <gibsonsgibson88@gmail.com>
2023-02-04 17:10:07 +0300

    ...................................

--- server/controllers/patientController.js
+++ server/controllers/patientController.js
@@ -1,11 +1,10 @@
 const asyncHandler = require('express-async-handler');
 const Patient = require('../models/patientModel');
+const healthRecords = require('../models/medicalHistory');
 
 const newPatient = asyncHandler(async (req, res) => {
     const { patientName, nationalId, patientAge, patientGender, patientPhone, patientEmail, visitedBefore } = req.body;
-
     //validate input data
-
     if (!patientName || !nationalId || !patientAge || !patientGender || !patientPhone || !patientEmail || !visitedBefore ) {
         res.status(400);
         throw new Error('Please fill all fields');
@@ -28,6 +27,7 @@ const newPatient = asyncHandler(async (req, res) => {
         //create new patient
         const patient = await Patient.create({
             patientAge, patientEmail, patientGender, patientName, patientPhone, nationalId, visitedBefore: { Date: new Date() }
+            
         });
         await patient.save();
         res.status(201).json({ message: 'Patient created successfully' });
