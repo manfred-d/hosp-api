9f163415ada988f134354fb1e49bcc9999d6d619
Domains18 <gibsonsgibson88@gmail.com>
2023-02-04 19:16:44 +0300

    server setup

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const colors = require('colors');
+const cors = require('cors');
+const path = require('path');
+const dotenv = require('dotenv');
+/*------------------------------------------end of dependencie--------------------------------------------------*/
+/*------------------------------------------setting up server----------------------------------------------*/
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+dotenv.config();
+const PORT = process.env.PORT
+/*------------------------------------------end of setting up server----------------------------------------------*/
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`.yellow.underline);
+});
