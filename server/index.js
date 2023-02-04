0f063443c672e8ad787cb23d929576a5a0179913
Domains18 <gibsonsgibson88@gmail.com>
2023-02-04 17:16:54 +0300

    server

--- server/index.js
+++ server/index.js
@@ -3,16 +3,17 @@ const express = require('express');
 const path = require("path");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const colors = require("colors");
 const mongoose = require("mongoose");
 const {errorHandler} = require("./handlers/errorHandler");
 const PORT = process.env.PORT;
-console.log(process.env.PORT);
+// console.log(process.env.PORT);
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 server.use(errorHandler);
-server.use(express.static(path.join(__dirname, "client/build")));
+// server.use(express.static(path.join(__dirname, "client/build")));
 
 
-server.listen(PORT, ()=>{ console.log(`Listening on port ${PORT}`)});
+server.listen(PORT, ()=>{ console.log(`Listening on port ${PORT}`.yellow.underline.underline.bold)});
