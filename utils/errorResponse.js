e96612b5632dc22112a07618bf7080bcf7949b41
Domains18 <gibsonsgibson88@gmail.com>
2023-02-05 22:10:38 +0300

    errorHandler

--- utils/errorResponse.js
+++ utils/errorResponse.js
@@ -1,7 +1,7 @@
 class ErrorResponse extends Error{
-    constructor(message, statuscode) {
+    constructor(message, statusCode) {
         super(message);
-        this.statuscode = statuscode;
+        this.statusCode = statusCode;
     }
 }
 
