f493e74bdc0178b818decf3156b522a596d62111
Domains18 <gibsonsgibson88@gmail.com>
2023-02-13 10:14:32 +0300

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,9 @@
 const User = require("../models/User");
 const Patient = require("../models/patient");
 const expressAsyncHandler = require('express-async-handler');
+const ErrorResponse = require("../utils/errorResponse");
+
+exports.register = expressAsyncHandler(async (req, res, next) => {
+    const user = await User.create(req.body);
+    
+});
