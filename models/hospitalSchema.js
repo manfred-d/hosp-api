86642a32780d046e02dc20a16b9a8c97639200c2
Domains18 <gibsonsgibson88@gmail.com>
2023-02-13 19:44:52 +0300

    hospital                        schema: complete

--- models/hospitalSchema.js
+++ models/hospitalSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require('bcryptjs');
+const expressAsyncHandler = require("express-async-handler");
 
 const hospitalSchema = new mongoose.Schema({
     name: {
@@ -124,3 +125,18 @@ const hospitalSchema = new mongoose.Schema({
         },
     ],   
 }, {timestamps: true});
+
+hospitalSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+hospitalSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("Hospitals", hospitalSchema);
