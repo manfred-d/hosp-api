c0f9dfae49fadade5ac9b06570767e36baec844f
Domains18 <gibsonsgibson88@gmail.com>
2023-02-14 17:19:36 +0300

    added doctor route with ts

---
+++ controllers/hospitalController.js
@@ -0,0 +1,5 @@
+const Hospitals = require("../models/hospitalSchema");
+const Doctors = require("../models/doctorSchema");
+
+const { sendNotification, playersId, externalUsersId } = require("./notificationController.js");
+
