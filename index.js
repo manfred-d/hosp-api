700dff4fe4e9334a63a23bf3c16dc93971f41dc3
Domains18 <gibsonsgibson88@gmail.com>
2023-02-14 23:54:57 +0300

    CRUD

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ const xss = require('xss-clean');
 const webpush = require('web-push');
 const bodyParser = require('body-parser');
 const app = express();
-
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
-
-
 //routes
 const hospitalRoutes = require('./routes/hospitalRoutes');
 const userRoutes = require('./routes/userRoute');
@@ -25,7 +22,6 @@ const appointmentRoutes = require('./routes/appointmentRoute');
 const doctorRoutes = require('./routes/doctorRoute');
 const Reports = require('./routes/report');
 const medicineRoutes = require('./routes/medicRoutes');
-
 //middlewares
 app.use(cors());
 app.use(morgan('common'));
@@ -36,22 +32,14 @@ app.use(helmet());
 app.use(mongoSanitize());
 app.use(xss());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
 app.use("/api/hospitals", hospitalRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/doctors", doctorRoutes);
 app.use("/api/reports", Reports);
 app.use("/api/medicines", medicineRoutes);
-
-
-
 connectDatabase();
-
 app.use(notFound);
 app.use(errorHandler);
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => { console.log(`Server running on port ${PORT}`.bold.yellow)});
