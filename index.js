1ba8f809680ace0ad40af2ddae7cd30bcf32ea5a
Domains18 <gibsonsgibson88@gmail.com>
2023-02-14 23:49:20 +0300

    complete backend

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const colors = require('colors');
+const dotenv = require('dotenv').config();
 const cors = require('cors');
-const path = require('path');
-const dotenv = require('dotenv');
-/*------------------------------------------end of dependencies--------------------------------------------------*/
-/*------------------------------------------setting up server----------------------------------------------*/
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-dotenv.config();
-const PORT = process.env.PORT
-/*------------------------------------------end of setting up server----------------------------------------------*/
-/*------------------------------------------setting up database----------------------------------------------*/
 const connectDatabase = require('./database/config');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+const expressSession = require('express-session');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const webpush = require('web-push');
+const bodyParser = require('body-parser');
+const app = express();
+
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+
+
+//routes
+const hospitalRoutes = require('./routes/hospitalRoutes');
+const userRoutes = require('./routes/userRoute');
+const appointmentRoutes = require('./routes/appointmentRoute');
+const doctorRoutes = require('./routes/doctorRoute');
+const Reports = require('./routes/report');
+const medicineRoutes = require('./routes/medicRoutes');
+
+//middlewares
+app.use(cors());
+app.use(morgan('common'));
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(expressSession({}));
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use("/api/hospitals", hospitalRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api/doctors", doctorRoutes);
+app.use("/api/reports", Reports);
+app.use("/api/medicines", medicineRoutes);
+
+
+
 connectDatabase();
 
-// routes
-app.use("/api", require("./routes"));
-// app.use("/api/doctor", require("./routes/Doctor"));
-// app.use("/api/report", require("./routes/Report"));
+app.use(notFound);
+app.use(errorHandler);
 
+const PORT = process.env.PORT || 5000;
 
-// listening port
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`.yellow.underline);
-});
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`.bold.yellow)});
